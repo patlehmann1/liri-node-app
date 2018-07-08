9f9f58d3b149fc04738213d93f975569cd9cc5ed
Patrick Lehmann <patlehmann1@gmail.com>
2018-07-08 13:02:32 -0400

    getting set up

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: 	MPXydUU6XSyp5hXMGdwdGrn5A,
-  consumer_secret: zCoAiYUefUlDxxROv5s3wV5yGpLU2SQnmyfC48KzebzBrTxps2,
-  access_token_key: 33423501-EdUqvTkG6vspmQ81JGGOqWxh6PIfhMJMsrpbDQWoL,
-  access_token_secret: Qp2m92aMkK0I75aJhsrkaVxifT4NUC2FtiPjI9F6ONnap
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
 exports.spotify = {
--- liri.js
+++ liri.js
@@ -1,3 +1,5 @@
-require("dotenv").config();
+var dotenv = require("dotenv").config();
+var keys = require("keys.js");
 
-var 
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
